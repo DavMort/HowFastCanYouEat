2ccb6b53ef6b07e40d6a16ecbd2aea6fa1b5f966
Jesper <jesper.thuresson@brightstraining.com>
2023-03-02 13:22:55 +0100

    relative css path and records ammend

--- HowFastCanEat/src/main/java/com/example/HowFastCanEat/Records.java
+++ HowFastCanEat/src/main/java/com/example/HowFastCanEat/Records.java
@@ -34,7 +34,7 @@ public class Records {
     public void sortList(ArrayList<Food> list) {
         for (int i = 0; i < list.size() - 1; i++) {
             Food temp = list.get(i);
-            if (list.get(i + 1).getKpm() > temp.getKpm()) {
+            if (list.get(i + 1).getNumberOfFood() > temp.getNumberOfFood()) {
                 list.remove(i);
                 list.add(temp);
                 i = 0;
